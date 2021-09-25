ef26a9a4708ac82398133f64212896ea437e3960
Tiago Nunes <ntiagon@gmail.com>
2021-09-25 18:44:25 -0300

    changed routes

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-const router = require('./routes');
+const usersRoutes = require('./router/usersRoutes');
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use(router);
+app.use(usersRoutes);
 
 app.listen(3333, () => {
   console.log('Back rodando na porta 3333');
