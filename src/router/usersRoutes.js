688092890a4b049be43cbd170e69eb16eec33c07
Tiago Nunes <ntiagon@gmail.com>
2021-09-25 19:18:37 -0300

    add token validation

--- src/router/usersRoutes.js
+++ src/router/usersRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const { userRegister, userLogin } = require('../controllers/users');
+const tokenValidation = require('../middlewares/tokenValidation');
 
 const userRouter = express();
 
 userRouter.post('/user/register', userRegister);
 userRouter.post('/user/login', userLogin);
+userRouter.use(tokenValidation);
 
 module.exports = userRouter;
