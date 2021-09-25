ebd473f9c02d6c60dac309b2997d66f06db859cc
Tiago Nunes <ntiagon@gmail.com>
2021-09-24 21:31:36 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.listen(3333, () => {
+  console.log('Back rodando na porta 3333');
+});
