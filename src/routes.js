6df20d073c4f1433dacd34b801a84cb13f183d3f
Tiago Nunes <ntiagon@gmail.com>
2021-09-24 21:43:55 -0300

    basic router

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,8 @@
-const router = require('express');
+const express = require('express');
+const { test } = require('./controllers/clients');
+
+const router = express();
+
+router.get('/', test);
+
+module.exports = router;
