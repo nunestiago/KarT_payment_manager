d84cdd80fca76c3efa3f57c7275ac71d5c713a59
Tiago Nunes <ntiagon@gmail.com>
2021-09-25 19:10:30 -0300

    dotenv

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const usersRoutes = require('./router/usersRoutes');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(cors());
 
 app.use(usersRoutes);
