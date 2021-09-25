8aaeccbaf21e2187eb43a7ae92407ae401109e98
Tiago Nunes <ntiagon@gmail.com>
2021-09-25 19:10:46 -0300

    login

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -43,4 +43,42 @@ const userRegister = async (req, res) => {
   }
 };
 
-module.exports = { userRegister };
+const userLogin = async (req, res) => {
+  const user = req.body;
+
+  const schema = yup.object().shape({
+    email: yup.string().email().required(),
+    senha: yup.string().required(),
+  });
+
+  try {
+    await schema.validate(req.body);
+
+    const isUser = await knex('usuarios').where('email', user.email);
+
+    if (!isUser.length) {
+      return res.status(400).json('Senha ou e-mail inválido.');
+    }
+
+    const foundUser = isUser[0];
+
+    const verifyPass = await bcrypt.compare(user.senha, foundUser.senha);
+    if (!verifyPass) {
+      return res.status(400).json('Senha ou e-mail inválido.');
+    }
+    const jwtSecret = process.env.TOKEN_SECRET;
+    const userSend = {
+      id: foundUser.id,
+      nome: foundUser.nome,
+      email: foundUser.email,
+    };
+    const token = jwt.sign(userSend, jwtSecret, {
+      expiresIn: process.env.TOKEN_EXPIRATION,
+    });
+    return res.status(200).json({ usuario: userSend, token });
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+module.exports = { userRegister, userLogin };
