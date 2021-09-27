6a8ee4e25eb6581ad1eb8da77e518b5d92d90e51
Tiago Nunes <ntiagon@gmail.com>
2021-09-27 19:21:39 -0300

    migrations

--- src/db/migrations/20210925233046_usuarios.js
+++ src/db/migrations/20210925233046_usuarios.js
@@ -1,8 +1,14 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable('usuarios', (table) => {
+    table.increments('id');
+    table.string('nome').notNullable();
+    table.string('email').notNullable();
+    table.string('senha').notNullable();
+    table.string('cpf', 11);
+    table.string('telefone', 11);
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable('usuarios');
 };
--- src/db/migrations/20210925233054_clientes.js
+++ src/db/migrations/20210925233054_clientes.js
@@ -1,8 +1,19 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable('clientes', (table) => {
+    table.increments('id');
+    table.string('nome').notNullable();
+    table.string('email').notNullable();
+    table.string('cpf', 11).notNullable();
+    table.string('telefone', 11).notNullable();
+    table.string('cep', 8);
+    table.string('logradouro');
+    table.string('bairro');
+    table.string('cidade');
+    table.string('complemento');
+    table.string('ponto_referencia');
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable('clientes');
 };
