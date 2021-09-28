6c01c9dc745ac2c971d89cd082db0d778d557415
Tiago Nunes <ntiagon@gmail.com>
2021-09-27 21:48:03 -0300

    delete Client method

--- src/controllers/clients.js
+++ src/controllers/clients.js
@@ -9,6 +9,7 @@ setLocale(pt);
 const clientRegister = async (req, res) => {
   const client = req.body;
   const { id } = req.user;
+
   client.usuario_id = id;
 
   const schema = yup.object().shape({
@@ -54,4 +55,27 @@ const clientRegister = async (req, res) => {
   }
 };
 
-module.exports = { clientRegister };
+const clienteDelete = async (req, res) => {
+  const { id } = req.params;
+  const userId = req.user;
+
+  try {
+    const isClient = await knex('clientes').where({ id, usuario_id: userId });
+
+    if (isClient.length) {
+      return res.status(400).json('Cliente não encontrado.');
+    }
+
+    const removeClient = await knex('clientes').where({ id }).del();
+
+    if (removeClient.length) {
+      return res.status(400).json('Não foi possível remover cliente.');
+    }
+
+    return res.status(200).json('Cliente removido com sucesso');
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+module.exports = { clientRegister, clienteDelete };
