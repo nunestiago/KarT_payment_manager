a1c24eccd499dc1a32ed275332e5208406440f0c
Tiago Nunes <ntiagon@gmail.com>
2021-09-28 21:31:32 -0300

    router change

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const routes = require('./router/routes');
+const routes = require('./router');
 
 const app = express();
 app.use(express.json());
