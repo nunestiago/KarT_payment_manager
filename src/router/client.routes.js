26507074e1fa8ba6d84e560801856abbf69f1185
Tiago Nunes <ntiagon@gmail.com>
2021-09-30 09:43:36 -0300

    add get clients

--- src/router/client.routes.js
+++ src/router/client.routes.js
@@ -1,9 +1,11 @@
 const { clienteDelete } = require('../controllers/clients/delete');
+const { clientGetAll } = require('../controllers/clients/getAll');
 const { clientRegister } = require('../controllers/clients/register');
 
 const clientRoutes = require('express').Router();
 
 clientRoutes.post('/client/register', clientRegister);
 clientRoutes.delete('/client/delete/:id', clienteDelete);
+clientRoutes.get('/client/getAll', clientGetAll);
 
 module.exports = clientRoutes;
