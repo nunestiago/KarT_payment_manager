56872237427f7765fae53a91fd487f61169def26
Tiago Nunes <ntiagon@gmail.com>
2021-10-04 16:23:44 -0300

    add charges routes

--- src/router/client.routes.js
+++ src/router/client.routes.js
@@ -1,3 +1,4 @@
+const { clientCharges } = require('../controllers/clients/charges');
 const { clienteDelete } = require('../controllers/clients/delete');
 const { clientGetAll } = require('../controllers/clients/getAll');
 const { clientRegister } = require('../controllers/clients/register');
@@ -7,5 +8,6 @@ const clientRoutes = require('express').Router();
 clientRoutes.post('/client/register', clientRegister);
 clientRoutes.delete('/client/delete/:id', clienteDelete);
 clientRoutes.get('/client/getAll', clientGetAll);
+clientRoutes.get('/client/getCharges', clientCharges);
 
 module.exports = clientRoutes;
