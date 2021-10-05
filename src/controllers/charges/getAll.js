94b5df472717159cbdde9bd0aaa9d3235dfb3e38
Tiago Nunes <ntiagon@gmail.com>
2021-10-05 11:33:14 -0300

    get charges method

---
+++ src/controllers/charges/getAll.js
@@ -0,0 +1,17 @@
+const knex = require('../../config/knexConnect');
+
+const clientCharges = async (req, res) => {
+  const { id } = req.user;
+
+  try {
+    const getCharges = await knex('cobrancas')
+      .where('usuario_id', id)
+      .orderBy('id', 'asc');
+
+    return res.status(200).json(getCharges);
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+module.exports = { clientCharges };
