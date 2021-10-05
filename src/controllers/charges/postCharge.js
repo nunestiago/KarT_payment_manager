eea4b8f7b3a634f6b0f1b37c0dc5a6334e88e624
Tiago Nunes <ntiagon@gmail.com>
2021-10-05 17:22:43 -0300

    new charge method

---
+++ src/controllers/charges/postCharge.js
@@ -0,0 +1,28 @@
+const knex = require('../../config/knexConnect');
+const chargeRegisterSchema = require('../../validations/chargeRegisterSchema');
+
+const postCharge = async (req, res) => {
+  const charge = req.body;
+  const { id } = req.user;
+
+  try {
+    await chargeRegisterSchema.validate(charge);
+    charge.usuario_id = id;
+    console.log(charge);
+    const newClient = await knex('cobrancas').insert(charge);
+
+    if (!newClient.rowCount) {
+      return res
+        .status(400)
+        .json(
+          'Erro ao tentar cadastrar cobrança, favor entrar em contato com suporte da KarT Devs.',
+        );
+    }
+
+    return res.status(200).json('Cobrança cadastrada com sucesso!');
+  } catch (error) {
+    return res.status(400).json(error.message);
+  }
+};
+
+module.exports = { postCharge };
