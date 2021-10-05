930630364e1f341f8b12672d6955d31a799cd1d5
Tiago Nunes <ntiagon@gmail.com>
2021-10-05 17:22:49 -0300

    updated routes

--- src/router/charges.routes.js
+++ src/router/charges.routes.js
@@ -1,9 +1,11 @@
 const { clientCharges } = require('../controllers/charges/getAll');
 const homeInfo = require('../controllers/charges/homeInfo');
+const { postCharge } = require('../controllers/charges/postCharge');
 
 const chargesRoutes = require('express').Router();
 
 chargesRoutes.get('/charges/getAll', clientCharges);
 chargesRoutes.get('/charges/homeInfo', homeInfo);
+chargesRoutes.post('/charges/newCharge', postCharge);
 
 module.exports = chargesRoutes;
