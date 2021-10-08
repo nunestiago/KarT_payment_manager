98b2051c2a711cb663ce6841b0e127e3ffe309d6
Tiago Nunes <ntiagon@gmail.com>
2021-10-08 19:18:21 -0300

    edit client fix

--- src/controllers/clients/editClient.js
+++ src/controllers/clients/editClient.js
@@ -1,36 +1,39 @@
 const bcrypt = require('bcrypt');
 const knex = require('../../config/knexConnect');
-const testeCPF = require('../../utils/cpfValidator');
+// const testeCPF = require('../../utils/cpfValidator');
 
 const editClient = async (req, res) => {
   const newInfo = req.body;
-  const { email, cpf } = newInfo;
+  const { email, cpf, id: idClient } = newInfo;
   const { id } = req.user;
-
   try {
-    const isClient = await knex('clientes').where({ usuario_id: id, email });
+    const oldClient = await knex('clientes').where({ id: idClient }).first();
 
-    if (isClient.length) {
-      return res.status(400).json('E-mail já cadastrado.');
+    if (oldClient.email !== email) {
+      const isClient = await knex('clientes').where({ usuario_id: id, email });
+      if (isClient.length) {
+        return res.status(400).json('E-mail já cadastrado.');
+      }
     }
-
-    const isCpf = await knex('clientes').where({ usuario_id: id, cpf });
-
-    if (isCpf.length) {
-      return res.status(400).json('CPF já cadastrado.');
+    if (oldClient.cpf !== cpf) {
+      const isCpf = await knex('clientes').where({ usuario_id: id, cpf });
+      if (isCpf.length) {
+        return res.status(400).json('CPF já cadastrado.');
+      }
     }
 
     if (newInfo.senha) {
       newInfo.senha = await bcrypt.hash(newInfo.senha, 10);
     }
 
-    if (newInfo.cpf && !testeCPF(newInfo.cpf)) {
-      return res.status(400).json('CPF inválido');
-    }
+    // if (newInfo.cpf && !testeCPF(newInfo.cpf)) {
+    //   return res.status(400).json('CPF inválido');
+    // }
 
     const editClient = await knex('clientes')
       .update(newInfo)
-      .where('id', newInfo.id);
+      .where('id', idClient);
+
     if (editClient !== 1) {
       return res.status(400).json('Não foi possível atualizar dados');
     }
