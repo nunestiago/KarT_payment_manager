15093ad971c96c695f1bcb2308a5dd84f6ccc966
Tiago Nunes <ntiagon@gmail.com>
2021-10-08 09:52:00 -0300

    new route client/listname

--- src/router/client.routes.js
+++ src/router/client.routes.js
@@ -1,5 +1,6 @@
 const { clienteDelete } = require('../controllers/clients/delete');
 const { clientGetAll } = require('../controllers/clients/getAll');
+const { listName } = require("../controllers/clients/listName");
 const { clientRegister } = require('../controllers/clients/register');
 
 const clientRoutes = require('express').Router();
@@ -7,5 +8,6 @@ const clientRoutes = require('express').Router();
 clientRoutes.post('/client/register', clientRegister);
 clientRoutes.delete('/client/delete/:id', clienteDelete);
 clientRoutes.get('/client/getAll', clientGetAll);
+clientRoutes.get('/client/listName', listName);
 
 module.exports = clientRoutes;
