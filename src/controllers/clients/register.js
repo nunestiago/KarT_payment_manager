539397a152fa57a835ee91c83ecb3c954a871a42
Tiago Nunes <ntiagon@gmail.com>
2021-10-08 16:07:44 -0300

    fixed double cpf and e-mail per client per user

--- src/controllers/clients/register.js
+++ src/controllers/clients/register.js
@@ -5,16 +5,22 @@ const clientRegisterSchema = require('../../validations/clientRegisterSchema');
 const clientRegister = async (req, res) => {
   const client = req.body;
   const { id } = req.user;
-
+  const { email, cpf } = client;
   client.usuario_id = id;
 
   try {
     await clientRegisterSchema.validate(client);
 
-    const isClient = await knex('clientes').where('email', client.email);
+    const isClient = await knex('clientes').where({ email, usuario_id: id });
 
     if (isClient.length) {
       return res.status(400).json('E-mail já cadastrado.');
+    }
+
+    const isCpf = await knex('clientes').where({ cpf, usuario_id: id });
+
+    if (isCpf.length) {
+      return res.status(400).json('CPF já cadastrado.');
     }
 
     if (client.cpf && !testeCPF(client.cpf)) {
