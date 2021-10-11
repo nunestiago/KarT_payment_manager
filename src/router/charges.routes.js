486847be0555ce3f4f226fa6496cb6423583aec4
Tiago Nunes <ntiagon@gmail.com>
2021-10-11 20:21:54 -0300

    updated routes

--- src/router/charges.routes.js
+++ src/router/charges.routes.js
@@ -1,3 +1,5 @@
+const { deleteCharge } = require('../controllers/charges/delete');
+const { editCharge } = require('../controllers/charges/editCharge');
 const { clientCharges } = require('../controllers/charges/getAll');
 const { getClientCharge } = require('../controllers/charges/getClientCharge');
 const homeInfo = require('../controllers/charges/homeInfo');
@@ -9,5 +11,7 @@ chargesRoutes.get('/charges/getAll', clientCharges);
 chargesRoutes.get('/charges/homeInfo', homeInfo);
 chargesRoutes.get('/charges/getCharge', getClientCharge);
 chargesRoutes.post('/charges/newCharge', postCharge);
+chargesRoutes.put('/charges/editCharge', editCharge);
+chargesRoutes.delete('/charges/delete', deleteCharge);
 
 module.exports = chargesRoutes;
