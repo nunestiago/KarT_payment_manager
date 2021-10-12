cc9935ec5e238ff8971f8c31d6683e99d3237a5e
Tiago Nunes <ntiagon@gmail.com>
2021-10-12 09:58:23 -0300

    not done yet

--- src/controllers/charges/delete.js
+++ src/controllers/charges/delete.js
@@ -1,24 +1,24 @@
-const knex = require('../../config/knexConnect');
-const chargeRegisterSchema = require('../../validations/chargeRegisterSchema');
+// const knex = require('../../config/knexConnect');
+// const chargeRegisterSchema = require('../../validations/chargeRegisterSchema');
 
-const deleteCharge = async (req, res) => {
-  const charge = req.body;
-  const { id } = req.user;
-  try {
-    const editCharge = await knex('cobrancas').where('id', charge.id).del();
+// const deleteCharge = async (req, res) => {
+//   const charge = req.body;
+//   const { id } = req.user;
+//   try {
+//     const editCharge = await knex('cobrancas').where('id', charge.id).del();
 
-    if (editCharge === 0) {
-      return res
-        .status(400)
-        .json(
-          'Erro ao tentar cadastrar cobrança, favor entrar em contato com suporte da KarT Devs.',
-        );
-    }
+//     if (editCharge === 0) {
+//       return res
+//         .status(400)
+//         .json(
+//           'Erro ao tentar cadastrar cobrança, favor entrar em contato com suporte da KarT Devs.',
+//         );
+//     }
 
-    return res.status(200).json('Cobrança editada com sucesso!');
-  } catch (error) {
-    return res.status(400).json(error.message);
-  }
-};
+//     return res.status(200).json('Cobrança editada com sucesso!');
+//   } catch (error) {
+//     return res.status(400).json(error.message);
+//   }
+// };
 
-module.exports = { deleteCharge };
+// module.exports = { deleteCharge };
